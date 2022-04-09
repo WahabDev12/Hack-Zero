547970f0cea23e066fb11bc46783e0a5180c2ada
WahabDev12 <nerdyshinobi12@gmail.com>
2022-04-09 17:25:23 +0000

    added sound attributes

---
+++ frontend/src/components/Chillax/Ocean.jsx
@@ -0,0 +1,25 @@
+import { Button } from "./Styles/Button.Styled";
+
+const Ocean = () => {
+    return ( 
+        <>
+         <Button>
+
+           <p className="attribute">
+               Ocean
+            </p>
+
+           <div className="card-icon">
+                    <span className="iconify" data-icon="bx:bx-water" style={{fontSize:"90px"}}></span>       
+           
+            </div>
+
+         </Button>
+
+
+        
+        </>
+     );
+}
+ 
+export default Ocean;
---
+++ frontend/src/components/Chillax/Thunder.jsx
@@ -0,0 +1,24 @@
+import { Button } from "./Styles/Button.Styled";
+
+const Thunder = () => {
+    return ( 
+        <>
+        <Button>
+
+          <p className="attribute">
+              Thunder
+           </p>
+
+          <div className="card-icon">
+                <ion-icon style={{fontSize:"90px"}}  name="flash-outline"></ion-icon>
+          </div>
+
+        </Button>
+
+
+       
+       </>
+     );
+}
+ 
+export default Thunder;
